47871b612e87bb4a7ad027b9e1da94c1a6bb7f81
Yevgeniy Sobolev <yevgeniyerot@gmail.com>
2019-02-23 06:15:39 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,13 +14,17 @@ def first_challenge
   }
 
   #your code here
-  contacts.collect do |key, value|
+  contact = contacts.collect do |key, value|
     if key == :favorite_icecream_flavors
       value.each do |element|
         if element == "strawberry"
-          value.delete
+          value.delete(element)
+        end
+      end
+    end
+  end
 
   #remember to return your newly altered contacts hash!
-  contacts
+  contact
 end
 
